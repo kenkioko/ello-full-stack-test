23a58645a956f88599c8b0d462caa4287a2de417
Kioko Kennedy <kmkioko@gmail.com>
2022-04-20 02:43:14 +0300

    feat: Switch Content And Tokenized
    Switch displayed content showing the tokenized text

--- virtual-rc/src/components/Content.js
+++ virtual-rc/src/components/Content.js
@@ -1,14 +1,46 @@
 import '../styles/Alert.css';
 
+function showTokenized(event) {
+    var children = event.currentTarget.children;
+    console.log(children);
+
+    for (let element of children) {
+        element.classList.toggle("d-none");
+        console.log(element);
+    }
+}
+
+function Tokenized(props) {
+    return (
+        <span>
+            { props.content.substring(
+                props.token.position[0],
+                props.token.position[1]
+            ) }
+        </span>
+    )
+}
+
 function Content(props) {
     if (!props.page) {
         return null;
     }
 
+    const tokenized = props.page.tokens.map((token, index) =>
+        <Tokenized key={ index } content={ props.page.content } token={ token } />
+    )
+
     return (
-        <div className="page-content">
-            { props.page.pageIndex }
-            { props.page.content }
+        <div className="page-content" onClick={ showTokenized }>
+            {/* { props.page.pageIndex } */}
+
+            <p className="page-text-content">
+                { props.page.content }
+            </p>
+
+            <p className="page-text-tokenized d-none">
+                { tokenized }
+            </p>
         </div>
     );
 }
